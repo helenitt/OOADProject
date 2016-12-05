82600e647e85405136c19097efbad7eba4b677fb
DanScallion <ifridius@gmail.com>
2016-12-05 21:26:51 +0100

    Modified BuyPiano to add use of PianoType

--- UC10/scr/BuyPiano.java
+++ UC10/scr/BuyPiano.java
@@ -11,24 +11,24 @@ public class BuyPiano {
 
     public BuyPiano(){
         pianos = new LinkedList<>();
-        addPiano(1, "Alden", 7000, 5);
-        addPiano(2, "Allmendiger", 7000, 1);
-        addPiano(3, "Berger", 7000, 15);
-        addPiano(4, "Dassel", 7000, 26);
-        addPiano(5, "Fazioli", 7000, 4);
-        addPiano(6, "Grover", 7000, 2);
+        addPiano(1, "Alden", PianoType.GRAND, 7000, 5);
+        addPiano(2, "Allmendiger", PianoType.ELECTRIC, 10500, 1);
+        addPiano(3, "Berger", PianoType.SPECIALIZED, 17000, 15);
+        addPiano(4, "Dassel", PianoType.UPRIGHT, 6500, 26);
+        addPiano(5, "Fazioli", PianoType.GRAND, 8500, 4);
+        addPiano(6, "Grover", PianoType.UPRIGHT, 7499, 2);
     }
 
     public static String getTitle(){
         return fTitle;
     }
 
-    public void addPiano(int pianoID, String pianoName, float pianoCost, int pianoQuantity){
-        Piano newPiano = new Piano(pianoID, pianoName, pianoCost, pianoQuantity);
+    public void addPiano(int pianoID, String pianoName, PianoType pianoType, float pianoCost, int pianoQuantity){
+        Piano newPiano = new Piano(pianoID, pianoName, pianoType, pianoCost, pianoQuantity);
         pianos.add(newPiano);
     }
 
     public void buyPiano(int pianoID, int pianoQuantity){
-        
+
     }
 }
