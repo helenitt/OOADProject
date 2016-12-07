f315b849df8e63f7e606f65fdba97c2e1fc879ed
patrickshaneclifford@gmail.com <tct3sah9>
2016-12-07 15:10:43 +0000

    removed more comments

--- InitialPianoTeacherSearch/src/PianoPerson.java
+++ InitialPianoTeacherSearch/src/PianoPerson.java
@@ -2,8 +2,7 @@
  * Created by t00183399 on 05/12/2016.
  */
 public interface PianoPerson {
-    //this will be an interface that should be used so that we can ensure we are programming to
-        //interfce not an implementation
+
 
          String getName();
          Locations getArea();
