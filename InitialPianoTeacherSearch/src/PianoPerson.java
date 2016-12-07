b82a58cc88b9ef7c4637554ea49a40b2b73df684
patrickshaneclifford@gmail.com <tct3sah9>
2016-12-07 14:40:42 +0000

    removed redundant 'public' modifiers in PianoPerson Interface

--- InitialPianoTeacherSearch/src/PianoPerson.java
+++ InitialPianoTeacherSearch/src/PianoPerson.java
@@ -5,13 +5,13 @@ public interface PianoPerson {
     //this will be an interface that should be used so that we can ensure we are programming to
         //interfce not an implementation
 
-        public String getName();
-        public String setName();
-        public String getArea();
-        public String setArea();
-        public String getPhone();
-        public String setPhone();
-        public String toString();
+         String getName();
+         String setName();
+         String getArea();
+         String setArea();
+         String getPhone();
+         String setPhone();
+         String toString();
 
 
 }
