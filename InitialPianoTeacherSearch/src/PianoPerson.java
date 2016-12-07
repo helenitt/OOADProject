6dd04f994ad7bb68aac49c67699065c6e56ef27e
patrickshaneclifford@gmail.com <tct3sah9>
2016-12-07 14:36:37 +0000

    Added the abstract methods to PianoPerson

--- InitialPianoTeacherSearch/src/PianoPerson.java
+++ InitialPianoTeacherSearch/src/PianoPerson.java
@@ -4,4 +4,14 @@
 public interface PianoPerson {
     //this will be an interface that should be used so that we can ensure we are programming to
         //interfce not an implementation
+
+        public String getName();
+        public String setName();
+        public String getArea();
+        public String setArea();
+        public String getPhone();
+        public String setPhone();
+        public String toString();
+
+
 }
