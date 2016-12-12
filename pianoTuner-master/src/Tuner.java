77c7243f8ab957efac0e9d6c3abf4623b1d28235
helenitt <helen.obrien2@students.ittralee.ie>
2016-12-12 09:29:20 +0000

    Commit unzipped version of Project Code
    Commit unzipped version of Project Code by Helen in to Group Repository

---
+++ pianoTuner-master/src/Tuner.java
@@ -0,0 +1,36 @@
+/**
+ * Created by Helen on 14/11/2016.
+ */
+public class Tuner {
+    final String name;
+    final Location location;
+    String phone;
+    Rating rating;
+
+    public Tuner(final String name,
+                 final Location location,
+                 String phone,
+                 Rating rating) {
+        this.name = name;
+        this.location = location;
+        this.phone = phone;
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public Enum getLocation() {
+        return location;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public Enum getRating() {
+        return rating;
+    }
+
+    public boolean mathches(Tuner matchSearch) {
+        return location == matchSearch.location && rating == matchSearch.rating;
+    }
+}
